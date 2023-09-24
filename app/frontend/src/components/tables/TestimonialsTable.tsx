18c624b18ab506fd04924569e5177a3431d96158
Zack Zellto <zacharyzellto@gmail.com>
2023-09-24 15:30:00 -0600

    added pagination to testimonials

--- app/frontend/src/components/tables/TestimonialsTable.tsx
+++ app/frontend/src/components/tables/TestimonialsTable.tsx
@@ -8,8 +8,19 @@ interface TableProps {
 }
 
 const TestimonialTable: React.FC<TableProps> = ({ headers, rows, icon }) => {
+  const [currentPage, setCurrentPage] = React.useState(1);
+  const rowsPerPage = 21;
+
+  const totalPages = Math.ceil(rows.length / rowsPerPage);
+  const maxPageButtons = Math.min(10, totalPages);
+  const startIndex = (currentPage - 1) * rowsPerPage;
+  const endIndex = startIndex + rowsPerPage;
+  const currentPageRows = rows.slice(startIndex, endIndex);
+
+  const pageNumbers = Array.from({ length: maxPageButtons }, (_, i) => i + 1);
+
   return (
-    <div className="flex justify-center overflow-x-auto drop-shadow-xl">
+    <div className="flex flex-col items-center overflow-x-auto drop-shadow-xl">
       <table className="mx-auto min-w-fit rounded-lg bg-slate-300 text-xs">
         <thead>
           <tr>
@@ -28,7 +39,7 @@ const TestimonialTable: React.FC<TableProps> = ({ headers, rows, icon }) => {
           </tr>
         </thead>
         <tbody className="font-normal break-words">
-          {rows.map((row, rowIndex) => (
+          {currentPageRows.map((row, rowIndex) => (
             <tr key={rowIndex}>
               {row.map((cell, cellIndex) => (
                 <td
@@ -60,6 +71,47 @@ const TestimonialTable: React.FC<TableProps> = ({ headers, rows, icon }) => {
           ))}
         </tbody>
       </table>
+      <div className="flex justify-center mt-4">
+        <button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage(1)}
+          className="mr-2 px-2 py-1 bg-gray-200 rounded"
+        >
+          First
+        </button>
+        <button
+          disabled={currentPage === 1}
+          onClick={() => setCurrentPage(currentPage - 1)}
+          className="mr-2 px-2 py-1 bg-gray-200 rounded"
+        >
+          Previous
+        </button>
+        {pageNumbers.map((number) => (
+          <button
+            key={number}
+            onClick={() => setCurrentPage(number)}
+            className={`px-2 py-1 bg-gray-200 rounded ${
+              currentPage === number ? "bg-gray-400" : ""
+            }`}
+          >
+            {number}
+          </button>
+        ))}
+        <button
+          disabled={currentPage === totalPages}
+          onClick={() => setCurrentPage(currentPage + 1)}
+          className="ml-2 px-2 py-1 bg-gray-200 rounded"
+        >
+          Next
+        </button>
+        <button
+          disabled={currentPage === totalPages}
+          onClick={() => setCurrentPage(totalPages)}
+          className="ml-2 px-2 py-1 bg-gray-200 rounded"
+        >
+          Last
+        </button>
+      </div>
     </div>
   );
 };
